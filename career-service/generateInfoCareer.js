a5f379a8193d831902ab8e2fbeb510842acd3efd
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-08-21 10:19:12 -0300

    [UPDATE] generate info career

--- career-service/generateInfoCareer.js
+++ career-service/generateInfoCareer.js
@@ -1,17 +1,21 @@
-const fs = require("fs");
+const fs = require("node:fs/promises");
 const { convertPdfToJson } = require("../pdf-service/pdf-service");
 
 async function generateInfoCareer({ pathPDF, gestion }) {
-  const { nameFileJson, content } = await convertPdfToJson(pathPDF);
+  try {
+    const { nameFileJson, content } = await convertPdfToJson(pathPDF);
 
-  fs.writeFileSync(
-    `./careers-datas-json/${gestion}/${nameFileJson}.json`,
-    JSON.stringify(content)
-  );
+    await fs.writeFile(
+      `./careers-datas-json/${gestion}/${nameFileJson}.json`,
+      JSON.stringify(content)
+    );
 
-  return {
-    nameFileJson,
-  };
+    return {
+      nameFileJson,
+    };
+  } catch (error) {
+    console.log("Error in generateInfoCareer: ", error);
+  }
 }
 
 module.exports = {
